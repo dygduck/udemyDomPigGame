8374a460ac6d5a89ab76d272e1c7b509b0ed59aa
duygu ebcim <duyguebcim@MacBook-Pro-2.local>
2019-03-01 11:04:00 +0300

    updating score and changing the active player

--- app.js
+++ app.js
@@ -33,6 +33,29 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
     // update the round score if the rolled number was not 1.
+
+    if(dice !== 1) {
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    } else {
+        //next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        // hide the dice
+        document.querySelector('.dice').style.display = 'none';
+        
+    }
 });
 
 
